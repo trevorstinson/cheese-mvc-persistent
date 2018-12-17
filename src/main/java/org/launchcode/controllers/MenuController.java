6a29c669ba6706d691bc7c8ecf3f5a1c3a24f010
Trevor Stinson <trevor@trevorstinson.com>
2018-12-17 15:30:48 -0600

    Set up menu index

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -5,6 +5,7 @@ import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,4 +17,14 @@ public class MenuController {
 
     @Autowired
     private MenuDao menuDao;
+
+    // Request path: /menu
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("menus", menuDao.findAll());
+        model.addAttribute("title", "Menus");
+
+        return "menu/index";
+    }
 }
