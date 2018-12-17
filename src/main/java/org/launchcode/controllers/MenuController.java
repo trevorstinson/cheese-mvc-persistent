fca3cde37a901ab688c1a83c4372972a2907acb0
Trevor Stinson <trevor@trevorstinson.com>
2018-12-17 15:22:32 -0600

    Create MenuController

---
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -0,0 +1,19 @@
+package org.launchcode.controllers;
+
+
+import org.launchcode.models.data.CheeseDao;
+import org.launchcode.models.data.MenuDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "menu")
+public class MenuController {
+
+    @Autowired
+    private CheeseDao cheeseDao;
+
+    @Autowired
+    private MenuDao menuDao;
+}
